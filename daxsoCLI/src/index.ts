37f978c83f735993490694546819147c48c9793e
xponentialai <haydenaylor911@gmail.com>
2023-08-28 17:53:07 -0500

    stashing

--- daxsoCLI/src/index.ts
+++ daxsoCLI/src/index.ts
@@ -2,29 +2,17 @@
 
 import { renderTitle } from "./../src/utils/renderTitle"
 import { logger } from "./../src/utils/logger";
-import { askQuestions, responseData } from "./store";
-import createFileStruct from "../plopfile";
-import { NodePlopAPI } from 'plop';
-import nodePlop from 'node-plop';
 
-const plop = nodePlop();
 
 process.stdin.resume();
 
 const main = async () => {
-//triggerAnalytics('start', 'started');
 
     renderTitle();
-    //triggerAnalytics('renderTitle', 'title');
-    //buildPkgInstallerMap(["devTest", "devTest2"])
-    await askQuestions();
-    await createFileStruct(await plop);
-    
+
     console.log("done")
+    process.exit(0)
 
-    //read the contents of response.json and write to .env
-
-    
 }
 main().catch((err) => {
   process.on('SIGINT', () => {});  // CTRL+C
