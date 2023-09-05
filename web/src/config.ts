3ddfa4b08e6e628af86b405664c19177b9f8e5be
xponentialai <haydenaylor911@gmail.com>
2023-09-05 18:41:04 -0500

    fixed path

--- web/src/config.ts
+++ web/src/config.ts
@@ -6,7 +6,7 @@ export const SITE = {
 
 export const OPEN_GRAPH = {
   image: {
-    src: "./../public/images/Thumbnail.jpg",
+    src: "./images/Thumbnail.jpg",
     alt: "Create Web3 App: The best way to start a new crypto project.",
   },
   twitter: "haydenaylor",
